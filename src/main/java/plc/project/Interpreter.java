7cbee5fa57d8f19685a9bd1a03bac50a607fa07e
MatthewKopelowitz <47190077+MatthewKopelowitz@users.noreply.github.com>
2022-10-25 16:56:16 -0400

    Added in lecture code/tests

---
+++ src/main/java/plc/project/Interpreter.java
@@ -0,0 +1,166 @@
+package plc.project;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
+
+    private Scope scope = new Scope(null);
+
+    public Interpreter(Scope parent) {
+        scope = new Scope(parent);
+        scope.defineFunction("print", 1, args -> {
+            System.out.println(args.get(0).getValue());
+            return Environment.NIL;
+        });
+
+        scope.defineFunction("logarithm", 1, args -> {
+            if (!(args.get(0).getValue() instanceof BigDecimal)) {
+                throw new RuntimeException("Expected a BigDecimal, received " + args.get(0).getValue().getClass().getName() + ".");
+            }
+
+            BigDecimal bd1 = (BigDecimal) args.get(0).getValue();
+
+            BigDecimal bd2 = requireType(BigDecimal.class, Environment.create(args.get(0).getValue()));
+
+            BigDecimal result = BigDecimal.valueOf(Math.log(bd2.doubleValue()));
+
+            return Environment.create(result);
+        });
+    }
+
+    public Scope getScope() {
+        return scope;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Source ast) {
+
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Global ast) {
+
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Function ast) {
+
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.Expression ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.Declaration ast) {
+
+        Optional optional = ast.getValue();
+        Boolean present = optional.isPresent();
+
+        if (present) {
+            Ast.Expression expr = (Ast.Expression) optional.get();
+
+            scope.defineVariable(ast.getName(), true, visit(expr));
+        } else {
+            scope.defineVariable(ast.getName(), true, Environment.NIL);
+        }
+
+        throw new UnsupportedOperationException(); //TODO (in lecture 10/25/22)
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.Assignment ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.If ast) {
+
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.Switch ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.Case ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.While ast) {
+        throw new UnsupportedOperationException(); //TODO (in lecture)
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Statement.Return ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expression.Literal ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expression.Group ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expression.Binary ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expression.Access ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expression.Function ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expression.PlcList ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    /**
+     * Helper function to ensure an object is of the appropriate type.
+     */
+    private static <T> T requireType(Class<T> type, Environment.PlcObject object) {
+        if (type.isInstance(object.getValue())) {
+            return type.cast(object.getValue());
+        } else {
+            throw new RuntimeException("Expected type " + type.getName() + ", received " + object.getValue().getClass().getName() + ".");
+        }
+    }
+
+    /**
+     * Exception class for returning values.
+     */
+    private static class Return extends RuntimeException {
+
+        private final Environment.PlcObject value;
+
+        private Return(Environment.PlcObject value) {
+            this.value = value;
+        }
+
+    }
+
+}
